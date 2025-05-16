fd58c20dbc45c6079986397ff89a2790f09ac87c
Alireza Sadeghi <alireza@Alirezas-MacBook-Pro.local>
2025-05-16 15:56:52 +0300

    backup

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }, // optional: skip if not using next/image
-  basePath: '/Portfolio',        // replace with your repo name
-  assetPrefix: '/Portfolio/',
   images: {
     domains: ['images.unsplash.com'],
     remotePatterns: [
